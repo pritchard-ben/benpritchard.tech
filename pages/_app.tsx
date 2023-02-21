0064b8c4cc80ac125fd680037c3b007d8cca6ddd
Ben Pritchard <benjhpritchard@gmail.com>
2023-02-21 21:57:25 +0000

    added navbar to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import NavBar from "@/components/NavBar";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <NavBar children={undefined} />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
