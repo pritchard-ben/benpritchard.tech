c803d69fd50beb8d990e7ad17c16b11841ba9066
Ben Pritchard <benjhpritchard@gmail.com>
2023-03-03 15:43:56 +0000

    Changed all files to fit new starter template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,27 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
-import NavBar from "@/components/NavBar";
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+import 'katex/dist/katex.css'
 
-const colors = {
-  brand: {
-    900: "#1a365d",
-  },
-};
+import '@fontsource/inter/variable-full.css'
 
-const theme = extendTheme({ colors });
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import siteMetadata from '@/data/siteMetadata'
+import Analytics from '@/components/analytics'
+import LayoutWrapper from '@/components/LayoutWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <NavBar children={undefined} />
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,104 @@
-import Head from "next/head";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import {
-  Box,
-  Flex,
-  Text,
-  useColorModeValue,
-  Image,
-  Center,
-} from "@chakra-ui/react";
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import formatDate from '@/lib/utils/formatDate'
+import { sortedBlogPost, allCoreContent } from '@/lib/utils/contentlayer'
+import { InferGetStaticPropsType } from 'next'
+import NewsletterForm from '@/components/NewsletterForm'
+import { allBlogs } from 'contentlayer/generated'
 
-const inter = Inter({ subsets: ["latin"] });
+const MAX_DISPLAY = 5
 
-export default function Home() {
+export const getStaticProps = async () => {
+  // TODO: move computation to get only the essential frontmatter to contentlayer.config
+  const sortedPosts = sortedBlogPost(allBlogs)
+  const posts = allCoreContent(sortedPosts)
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      <Head>
-        <title>Ben Pritchard</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/img/logo.png" />
-      </Head>
-      <main className={styles.main}>
-        <div
-          className={styles.container}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "300px",
-          }}
-        >
-          <Box w="100%" h="300px" bgImage="/img/cover-image(1).png">
-            <Image
-              src="/img/profilepic.png"
-              alt="logo"
-              width={200}
-              height={200}
-              borderRadius="full"
-            />
-          </Box>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
         </div>
-      </main>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((post) => {
+            const { slug, date, title, summary, tags } = post
+            return (
+              <li key={slug} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+      {siteMetadata.newsletter.provider !== '' && (
+        <div className="flex items-center justify-center pt-4">
+          <NewsletterForm />
+        </div>
+      )}
     </>
-  );
+  )
 }
